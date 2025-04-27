a88b846a9775312861b99ec88181d9325e3885b8
JacksonKaunismaa <jackkaunis@protonmail.com>
2025-04-27 15:27:13 -0700

    rm auto repo clone

--- custom_bins/setup-runpod-global.sh
+++ custom_bins/setup-runpod-global.sh
@@ -8,26 +8,3 @@ yes | curl -LsSf https://astral.sh/uv/install.sh | sh
 echo "Finished deploying"
 apt-get install -y vim
 apt-get install -y screen
-
-
-# Ask about repository cloning
-repo_url="git@github.com:jplhughes/alm-jailbreaks.git" 
-# Ask where to clone
-
-git clone "$repo_url"
-repo_name=$(basename "$repo_url" .git)
-cd "$repo_name"
-
-echo "Setup complete!"
-
-echo "fully finished setup-runpod"
-
-# add uv to path
-source $HOME/.local/bin/env
-uv venv --python 3.11
-source .venv/bin/activate
-uv pip install -r requirements.txt
-uv pip install -e .
-./scripts/ft_deps.sh
-
-/start.sh
