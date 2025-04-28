665ec0ae94c2593100a492d346b4efcae8183086
JacksonKaunismaa <jackkaunis@protonmail.com>
2025-04-27 18:22:23 -0700

    add postdeploy script

---
+++ custom_bins/post-deploy-setup-runpod.sh
@@ -0,0 +1,13 @@
+git clone git@github.com:jplhughes/alm-jailbreaks.git
+
+cd alm-jailbreaks
+
+# add uv to path
+source $HOME/.local/bin/env
+rm -rf .venv
+uv venv --python 3.11
+source .venv/bin/activate
+uv pip install -r requirements.txt
+uv pip install -e .
+./scripts/ft_deps.sh
+
