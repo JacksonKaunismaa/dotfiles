f41ffdbf3b3c30a415456fc8d9f3ed067ac13b99
JacksonKaunismaa <jackkaunis@protonmail.com>
2025-05-11 06:08:21 +0000

    make deploy script better

--- custom_bins/post-deploy-setup-runpod.sh
+++ custom_bins/post-deploy-setup-runpod.sh
@@ -1,13 +1,15 @@
+cd $HOME
 git clone git@github.com:jplhughes/alm-jailbreaks.git
-git checkout web_interface
 cd alm-jailbreaks
+git checkout ft_synthetic_data
 
 # add uv to path
+cwd=$(pwd)
 source $HOME/.local/bin/env
-rm -rf .venv
-uv venv --python 3.11
-source .venv/bin/activate
-uv pip install -r requirements.txt
-uv pip install -e .
-./scripts/ft_deps.sh
+rm -rf $cwd/.venv
+uv venv --python 3.11 $cwd
+source $cwd/.venv/bin/activate
+uv pip install -r $cwd/requirements.txt
+uv pip install -e $cwd
+$cwd/scripts/ft_deps.sh
 
