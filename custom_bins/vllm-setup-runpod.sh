80b15fa170c601f4f37ecef7e4fd7ecf916b25ea
JacksonKaunismaa <jackkaunis@protonmail.com>
2025-08-03 11:05:06 -0400

    add installation stuff to vllm setup

--- custom_bins/vllm-setup-runpod.sh
+++ custom_bins/vllm-setup-runpod.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+echo "Running assuming that cwd is ~/dotfiles"
 git remote remove origin
 git remote add origin git@github.com:JacksonKaunismaa/dotfiles.git
-cd $HOME
+
+./install.sh --tmux --zsh --extras --is-root
+./deploy.sh
+echo "Installing uv..."
+yes | curl -LsSf https://astral.sh/uv/install.sh | sh
+echo "Finished deploying"
+apt-get install -y vim
+echo "Installing screen..."
+apt-get install -y screen
+echo "Done installing deps!"
 
 # add uv to path
 source $HOME/.local/bin/env
@@ -12,3 +23,5 @@ uv pip install vllm --torch-backend=auto
 uv pip install flash-attn --no-build-isolation
 uv pip install transformers accelerate bitsandbytes huggingface_hub datasets fastapi uvicorn pydantic
 ln -s $PWD/custom_bins/launch-vllm.sh $HOME/launch-vllm.sh
+
+/start.sh
