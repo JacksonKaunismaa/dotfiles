296ba7facd732fb4f4ad99f70210a33979eb74b4
JacksonKaunismaa <jackkaunis@protonmail.com>
2025-08-11 15:59:36 -0400

    mv gptqmodel to no build isolation

--- custom_bins/runpod-serverless-setup.sh
+++ custom_bins/runpod-serverless-setup.sh
@@ -21,4 +21,5 @@ source $HOME/.local/bin/env
 rm -rf $HOME/.venv
 uv venv --python 3.12 $HOME/.venv
 source $HOME/.venv/bin/activate
-uv pip transformers transformer_lens torch optimum peft ipykernel gptqmodel logbar threadpoolctl tokenicer device_smi Pillow 
+uv pip install transformers transformer_lens torch optimum peft ipykernel logbar threadpoolctl tokenicer device_smi Pillow 
+uv pip install gptqmodel --no-build-isolation
